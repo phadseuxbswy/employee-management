0ea285cf5e2f3c9f3e260508fbf62c73c4106e4a
pattarapa <phadseuxbswy@gmail.com>
2026-09-14 21:45:25 +0700

    Fix settings import in urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from myproject.myproject import settings
+from django.conf import settings
 from . import views
 from django.conf import settings
 from django.urls import path, re_path
